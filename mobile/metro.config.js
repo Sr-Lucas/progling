ed1e02e52ce4854124802c5a3d6631d886dca46f
Lucas Santos <lucas.santos@premiersoft.net>
2023-08-28 19:38:52 -0300

    chore: svg config

--- mobile/metro.config.js
+++ mobile/metro.config.js
@@ -1,7 +1,21 @@
 const { getDefaultConfig } = require("expo/metro-config");
 
-const config = getDefaultConfig(__dirname, {
-  isCSSEnabled: true,
-});
+module.exports = (() => {
+  const config = getDefaultConfig(__dirname, {
+    isCSSEnabled: true,
+  });
 
-module.exports = config;
+  const { transformer, resolver } = config;
+
+  config.transformer = {
+    ...transformer,
+    babelTransformerPath: require.resolve("react-native-svg-transformer")
+  };
+  config.resolver = {
+    ...resolver,
+    assetExts: resolver.assetExts.filter((ext) => ext !== "svg"),
+    sourceExts: [...resolver.sourceExts, "svg"]
+  };
+
+  return config;
+})();
