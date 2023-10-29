7a401a1ad04ba0a2b82058d357939b7f22abc0b2
Lucas <lucastsantos.developer@gmail.com>
2023-10-29 20:45:37 -0300

    chore: docker prod config

---
+++ api/prisma/migrations/20231029215722_v001/migration.sql
@@ -0,0 +1,260 @@
+-- CreateTable
+CREATE TABLE `Image` (
+    `id` VARCHAR(191) NOT NULL,
+    `name` VARCHAR(191) NOT NULL,
+    `storeType` VARCHAR(191) NOT NULL DEFAULT 'local',
+    `createdAt` DATETIME(3) NOT NULL DEFAULT CURRENT_TIMESTAMP(3),
+    `updatedAt` DATETIME(3) NOT NULL,
+
+    PRIMARY KEY (`id`)
+) DEFAULT CHARACTER SET utf8mb4 COLLATE utf8mb4_unicode_ci;
+
+-- CreateTable
+CREATE TABLE `Student` (
+    `id` VARCHAR(191) NOT NULL,
+    `email` VARCHAR(191) NOT NULL,
+    `name` VARCHAR(191) NOT NULL,
+    `password` VARCHAR(191) NOT NULL,
+    `hearts` INTEGER NOT NULL DEFAULT 3,
+    `heartsRenewAt` DATETIME(3) NULL,
+    `streak` INTEGER NOT NULL DEFAULT 0,
+    `streakRenewAt` DATETIME(3) NULL,
+    `streakResetAt` DATETIME(3) NULL,
+    `createdAt` DATETIME(3) NOT NULL DEFAULT CURRENT_TIMESTAMP(3),
+    `updatedAt` DATETIME(3) NOT NULL,
+    `deletedAt` DATETIME(3) NULL,
+
+    UNIQUE INDEX `Student_email_key`(`email`),
+    PRIMARY KEY (`id`)
+) DEFAULT CHARACTER SET utf8mb4 COLLATE utf8mb4_unicode_ci;
+
+-- CreateTable
+CREATE TABLE `ProgrammingLanguage` (
+    `id` VARCHAR(191) NOT NULL,
+    `name` VARCHAR(191) NOT NULL,
+    `description` VARCHAR(191) NOT NULL,
+    `imageId` VARCHAR(191) NOT NULL,
+    `createdAt` DATETIME(3) NOT NULL DEFAULT CURRENT_TIMESTAMP(3),
+    `updatedAt` DATETIME(3) NOT NULL,
+
+    UNIQUE INDEX `ProgrammingLanguage_imageId_key`(`imageId`),
+    PRIMARY KEY (`id`)
+) DEFAULT CHARACTER SET utf8mb4 COLLATE utf8mb4_unicode_ci;
+
+-- CreateTable
+CREATE TABLE `Module` (
+    `id` VARCHAR(191) NOT NULL,
+    `name` VARCHAR(191) NOT NULL,
+    `description` TEXT NOT NULL,
+    `createdAt` DATETIME(3) NOT NULL DEFAULT CURRENT_TIMESTAMP(3),
+    `updatedAt` DATETIME(3) NOT NULL,
+    `programmingLanguageId` VARCHAR(191) NOT NULL,
+    `orderNumber` INTEGER NOT NULL DEFAULT 0,
+
+    UNIQUE INDEX `Module_orderNumber_key`(`orderNumber`),
+    PRIMARY KEY (`id`)
+) DEFAULT CHARACTER SET utf8mb4 COLLATE utf8mb4_unicode_ci;
+
+-- CreateTable
+CREATE TABLE `Level` (
+    `id` VARCHAR(191) NOT NULL,
+    `name` VARCHAR(191) NOT NULL,
+    `description` TEXT NOT NULL,
+    `createdAt` DATETIME(3) NOT NULL DEFAULT CURRENT_TIMESTAMP(3),
+    `updatedAt` DATETIME(3) NOT NULL,
+    `orderNumber` INTEGER NOT NULL DEFAULT 0,
+    `moduleId` VARCHAR(191) NOT NULL,
+
+    PRIMARY KEY (`id`)
+) DEFAULT CHARACTER SET utf8mb4 COLLATE utf8mb4_unicode_ci;
+
+-- CreateTable
+CREATE TABLE `StudentAnswer` (
+    `id` VARCHAR(191) NOT NULL,
+    `createdAt` DATETIME(3) NOT NULL DEFAULT CURRENT_TIMESTAMP(3),
+    `updatedAt` DATETIME(3) NOT NULL,
+    `studentId` VARCHAR(191) NOT NULL,
+    `miniGameId` VARCHAR(191) NOT NULL,
+    `codeOrderingMiniGameStudentAnswerId` VARCHAR(191) NULL,
+
+    PRIMARY KEY (`id`)
+) DEFAULT CHARACTER SET utf8mb4 COLLATE utf8mb4_unicode_ci;
+
+-- CreateTable
+CREATE TABLE `TrueFalseMiniGameStudentAnswer` (
+    `id` VARCHAR(191) NOT NULL,
+    `createdAt` DATETIME(3) NOT NULL DEFAULT CURRENT_TIMESTAMP(3),
+    `updatedAt` DATETIME(3) NOT NULL,
+    `studentAnswerId` VARCHAR(191) NOT NULL,
+    `answer` BOOLEAN NOT NULL,
+
+    UNIQUE INDEX `TrueFalseMiniGameStudentAnswer_studentAnswerId_key`(`studentAnswerId`),
+    PRIMARY KEY (`id`)
+) DEFAULT CHARACTER SET utf8mb4 COLLATE utf8mb4_unicode_ci;
+
+-- CreateTable
+CREATE TABLE `CodeCompletionMiniGameStudentAnswer` (
+    `id` VARCHAR(191) NOT NULL,
+    `createdAt` DATETIME(3) NOT NULL DEFAULT CURRENT_TIMESTAMP(3),
+    `updatedAt` DATETIME(3) NOT NULL,
+    `studentAnswerId` VARCHAR(191) NOT NULL,
+    `answer` VARCHAR(191) NOT NULL,
+
+    UNIQUE INDEX `CodeCompletionMiniGameStudentAnswer_studentAnswerId_key`(`studentAnswerId`),
+    PRIMARY KEY (`id`)
+) DEFAULT CHARACTER SET utf8mb4 COLLATE utf8mb4_unicode_ci;
+
+-- CreateTable
+CREATE TABLE `CodeOrderingMiniGameStudentAnswer` (
+    `id` VARCHAR(191) NOT NULL,
+    `createdAt` DATETIME(3) NOT NULL DEFAULT CURRENT_TIMESTAMP(3),
+    `updatedAt` DATETIME(3) NOT NULL,
+    `studentAnswerId` VARCHAR(191) NOT NULL,
+
+    PRIMARY KEY (`id`)
+) DEFAULT CHARACTER SET utf8mb4 COLLATE utf8mb4_unicode_ci;
+
+-- CreateTable
+CREATE TABLE `CodeOrderingMiniGameStudentAnswerOption` (
+    `id` VARCHAR(191) NOT NULL,
+    `createdAt` DATETIME(3) NOT NULL DEFAULT CURRENT_TIMESTAMP(3),
+    `updatedAt` DATETIME(3) NOT NULL,
+    `studentAnswerId` VARCHAR(191) NOT NULL,
+    `answerOrder` INTEGER NOT NULL,
+    `correctOrder` INTEGER NOT NULL,
+    `codeOrderingMiniGameStudentAnswerId` VARCHAR(191) NULL,
+
+    PRIMARY KEY (`id`)
+) DEFAULT CHARACTER SET utf8mb4 COLLATE utf8mb4_unicode_ci;
+
+-- CreateTable
+CREATE TABLE `MiniGame` (
+    `id` VARCHAR(191) NOT NULL,
+    `type` VARCHAR(191) NOT NULL,
+    `createdAt` DATETIME(3) NOT NULL DEFAULT CURRENT_TIMESTAMP(3),
+    `updatedAt` DATETIME(3) NOT NULL,
+    `levelId` VARCHAR(191) NOT NULL,
+    `markdownMiniGameId` VARCHAR(191) NULL,
+    `trueFalseMiniGameId` VARCHAR(191) NULL,
+    `codeCompletionMiniGameId` VARCHAR(191) NULL,
+    `codeOrderingMiniGameId` VARCHAR(191) NULL,
+    `orderNumber` INTEGER NOT NULL DEFAULT 0,
+
+    UNIQUE INDEX `MiniGame_markdownMiniGameId_key`(`markdownMiniGameId`),
+    UNIQUE INDEX `MiniGame_trueFalseMiniGameId_key`(`trueFalseMiniGameId`),
+    UNIQUE INDEX `MiniGame_codeCompletionMiniGameId_key`(`codeCompletionMiniGameId`),
+    UNIQUE INDEX `MiniGame_codeOrderingMiniGameId_key`(`codeOrderingMiniGameId`),
+    PRIMARY KEY (`id`)
+) DEFAULT CHARACTER SET utf8mb4 COLLATE utf8mb4_unicode_ci;
+
+-- CreateTable
+CREATE TABLE `MarkdownMiniGame` (
+    `id` VARCHAR(191) NOT NULL,
+    `markdown` MEDIUMTEXT NOT NULL,
+    `createdAt` DATETIME(3) NOT NULL DEFAULT CURRENT_TIMESTAMP(3),
+    `updatedAt` DATETIME(3) NOT NULL,
+
+    PRIMARY KEY (`id`)
+) DEFAULT CHARACTER SET utf8mb4 COLLATE utf8mb4_unicode_ci;
+
+-- CreateTable
+CREATE TABLE `TrueFalseMiniGame` (
+    `id` VARCHAR(191) NOT NULL,
+    `question` TEXT NOT NULL,
+    `correctAnswer` BOOLEAN NOT NULL,
+    `createdAt` DATETIME(3) NOT NULL DEFAULT CURRENT_TIMESTAMP(3),
+    `updatedAt` DATETIME(3) NOT NULL,
+
+    PRIMARY KEY (`id`)
+) DEFAULT CHARACTER SET utf8mb4 COLLATE utf8mb4_unicode_ci;
+
+-- CreateTable
+CREATE TABLE `CodeCompletionMiniGame` (
+    `id` VARCHAR(191) NOT NULL,
+    `code` VARCHAR(191) NOT NULL,
+    `createdAt` DATETIME(3) NOT NULL DEFAULT CURRENT_TIMESTAMP(3),
+    `updatedAt` DATETIME(3) NOT NULL,
+
+    PRIMARY KEY (`id`)
+) DEFAULT CHARACTER SET utf8mb4 COLLATE utf8mb4_unicode_ci;
+
+-- CreateTable
+CREATE TABLE `CodeOrderingMiniGame` (
+    `id` VARCHAR(191) NOT NULL,
+    `createdAt` DATETIME(3) NOT NULL DEFAULT CURRENT_TIMESTAMP(3),
+    `updatedAt` DATETIME(3) NOT NULL,
+
+    PRIMARY KEY (`id`)
+) DEFAULT CHARACTER SET utf8mb4 COLLATE utf8mb4_unicode_ci;
+
+-- CreateTable
+CREATE TABLE `CodeCompletionMiniGameOption` (
+    `id` VARCHAR(191) NOT NULL,
+    `content` VARCHAR(191) NOT NULL,
+    `isCorrect` BOOLEAN NOT NULL,
+    `createdAt` DATETIME(3) NOT NULL DEFAULT CURRENT_TIMESTAMP(3),
+    `updatedAt` DATETIME(3) NOT NULL,
+    `codeCompletionMiniGameId` VARCHAR(191) NOT NULL,
+
+    PRIMARY KEY (`id`)
+) DEFAULT CHARACTER SET utf8mb4 COLLATE utf8mb4_unicode_ci;
+
+-- CreateTable
+CREATE TABLE `CodeOrderingMiniGameOption` (
+    `id` VARCHAR(191) NOT NULL,
+    `content` VARCHAR(191) NOT NULL,
+    `order` INTEGER NOT NULL,
+    `createdAt` DATETIME(3) NOT NULL DEFAULT CURRENT_TIMESTAMP(3),
+    `updatedAt` DATETIME(3) NOT NULL,
+    `codeOrderingMiniGameId` VARCHAR(191) NOT NULL,
+
+    PRIMARY KEY (`id`)
+) DEFAULT CHARACTER SET utf8mb4 COLLATE utf8mb4_unicode_ci;
+
+-- AddForeignKey
+ALTER TABLE `ProgrammingLanguage` ADD CONSTRAINT `ProgrammingLanguage_imageId_fkey` FOREIGN KEY (`imageId`) REFERENCES `Image`(`id`) ON DELETE RESTRICT ON UPDATE CASCADE;
+
+-- AddForeignKey
+ALTER TABLE `Module` ADD CONSTRAINT `Module_programmingLanguageId_fkey` FOREIGN KEY (`programmingLanguageId`) REFERENCES `ProgrammingLanguage`(`id`) ON DELETE RESTRICT ON UPDATE CASCADE;
+
+-- AddForeignKey
+ALTER TABLE `Level` ADD CONSTRAINT `Level_moduleId_fkey` FOREIGN KEY (`moduleId`) REFERENCES `Module`(`id`) ON DELETE RESTRICT ON UPDATE CASCADE;
+
+-- AddForeignKey
+ALTER TABLE `StudentAnswer` ADD CONSTRAINT `StudentAnswer_studentId_fkey` FOREIGN KEY (`studentId`) REFERENCES `Student`(`id`) ON DELETE RESTRICT ON UPDATE CASCADE;
+
+-- AddForeignKey
+ALTER TABLE `StudentAnswer` ADD CONSTRAINT `StudentAnswer_miniGameId_fkey` FOREIGN KEY (`miniGameId`) REFERENCES `MiniGame`(`id`) ON DELETE RESTRICT ON UPDATE CASCADE;
+
+-- AddForeignKey
+ALTER TABLE `StudentAnswer` ADD CONSTRAINT `StudentAnswer_codeOrderingMiniGameStudentAnswerId_fkey` FOREIGN KEY (`codeOrderingMiniGameStudentAnswerId`) REFERENCES `CodeOrderingMiniGameStudentAnswer`(`id`) ON DELETE SET NULL ON UPDATE CASCADE;
+
+-- AddForeignKey
+ALTER TABLE `TrueFalseMiniGameStudentAnswer` ADD CONSTRAINT `TrueFalseMiniGameStudentAnswer_studentAnswerId_fkey` FOREIGN KEY (`studentAnswerId`) REFERENCES `StudentAnswer`(`id`) ON DELETE RESTRICT ON UPDATE CASCADE;
+
+-- AddForeignKey
+ALTER TABLE `CodeCompletionMiniGameStudentAnswer` ADD CONSTRAINT `CodeCompletionMiniGameStudentAnswer_studentAnswerId_fkey` FOREIGN KEY (`studentAnswerId`) REFERENCES `StudentAnswer`(`id`) ON DELETE RESTRICT ON UPDATE CASCADE;
+
+-- AddForeignKey
+ALTER TABLE `CodeOrderingMiniGameStudentAnswerOption` ADD CONSTRAINT `CodeOrderingMiniGameStudentAnswerOption_codeOrderingMiniGam_fkey` FOREIGN KEY (`codeOrderingMiniGameStudentAnswerId`) REFERENCES `CodeOrderingMiniGameStudentAnswer`(`id`) ON DELETE SET NULL ON UPDATE CASCADE;
+
+-- AddForeignKey
+ALTER TABLE `MiniGame` ADD CONSTRAINT `MiniGame_levelId_fkey` FOREIGN KEY (`levelId`) REFERENCES `Level`(`id`) ON DELETE RESTRICT ON UPDATE CASCADE;
+
+-- AddForeignKey
+ALTER TABLE `MiniGame` ADD CONSTRAINT `MiniGame_trueFalseMiniGameId_fkey` FOREIGN KEY (`trueFalseMiniGameId`) REFERENCES `TrueFalseMiniGame`(`id`) ON DELETE SET NULL ON UPDATE CASCADE;
+
+-- AddForeignKey
+ALTER TABLE `MiniGame` ADD CONSTRAINT `MiniGame_codeCompletionMiniGameId_fkey` FOREIGN KEY (`codeCompletionMiniGameId`) REFERENCES `CodeCompletionMiniGame`(`id`) ON DELETE SET NULL ON UPDATE CASCADE;
+
+-- AddForeignKey
+ALTER TABLE `MiniGame` ADD CONSTRAINT `MiniGame_codeOrderingMiniGameId_fkey` FOREIGN KEY (`codeOrderingMiniGameId`) REFERENCES `CodeOrderingMiniGame`(`id`) ON DELETE SET NULL ON UPDATE CASCADE;
+
+-- AddForeignKey
+ALTER TABLE `MiniGame` ADD CONSTRAINT `MiniGame_markdownMiniGameId_fkey` FOREIGN KEY (`markdownMiniGameId`) REFERENCES `MarkdownMiniGame`(`id`) ON DELETE SET NULL ON UPDATE CASCADE;
+
+-- AddForeignKey
+ALTER TABLE `CodeCompletionMiniGameOption` ADD CONSTRAINT `CodeCompletionMiniGameOption_codeCompletionMiniGameId_fkey` FOREIGN KEY (`codeCompletionMiniGameId`) REFERENCES `CodeCompletionMiniGame`(`id`) ON DELETE RESTRICT ON UPDATE CASCADE;
+
+-- AddForeignKey
+ALTER TABLE `CodeOrderingMiniGameOption` ADD CONSTRAINT `CodeOrderingMiniGameOption_codeOrderingMiniGameId_fkey` FOREIGN KEY (`codeOrderingMiniGameId`) REFERENCES `CodeOrderingMiniGame`(`id`) ON DELETE RESTRICT ON UPDATE CASCADE;
--- api/prisma/seeder/data/minigames/module_2_minigames.ts
+++ api/prisma/seeder/data/minigames/module_2_minigames.ts
@@ -1,4 +1,10 @@
-import { MarkdownMiniGame, MiniGame, TrueFalseMiniGame } from '@prisma/client';
+import {
+  CodeCompletionMiniGame,
+  CodeCompletionMiniGameOption,
+  MarkdownMiniGame,
+  MiniGame,
+  TrueFalseMiniGame,
+} from '@prisma/client';
 import { levels } from '../levels';
 import { module2Mds } from './makdowns/module2-mds';
 
@@ -10,6 +16,114 @@ export const module2MarkDownMiniGames: MarkdownMiniGame[] = [
     updatedAt: new Date(),
   },
 ];
+
+export const module2CodeCompletionMiniGames: CodeCompletionMiniGame[] = [
+  {
+    id: '6256133f-a0cb-4fec-b25c-520595ddf884',
+    code: `\n const age = 18;\n\n _20_ (age < 18) {\n   console.log('Você é menor de idade.');\n }\n\n`,
+    createdAt: new Date(),
+    updatedAt: new Date(),
+  },
+  {
+    id: 'c72d7fe7-8a45-433a-9cab-f625248c53e6',
+    code: `\n const age = 18;\n\n if (age < 18) {\n   console.log('Você é menor de idade.');\n } _20_ {\n   console.log('Você é maior de idade.');\n }\n\n`,
+    createdAt: new Date(),
+    updatedAt: new Date(),
+  },
+];
+
+export const module2CodeCompletionMiniGameOptions: CodeCompletionMiniGameOption[] =
+  [
+    // Code Completion - 1 (6256133f-a0cb-4fec-b25c-520595ddf884)
+    ...[
+      {
+        id: '1249d95f-fe5f-458f-96ff-4457eddf0067',
+        codeCompletionMiniGameId: '6256133f-a0cb-4fec-b25c-520595ddf884',
+        content: 'elif',
+        isCorrect: false,
+        createdAt: new Date(),
+        updatedAt: new Date(),
+      },
+      {
+        id: '4d422971-1925-42a1-b2d1-5291143b8c0b',
+        codeCompletionMiniGameId: '6256133f-a0cb-4fec-b25c-520595ddf884',
+        content: 'se',
+        isCorrect: true,
+        createdAt: new Date(),
+        updatedAt: new Date(),
+      },
+      {
+        id: 'af885ab7-e708-48f3-9b9b-5634878851bc',
+        codeCompletionMiniGameId: '6256133f-a0cb-4fec-b25c-520595ddf884',
+        content: 'while',
+        isCorrect: false,
+        createdAt: new Date(),
+        updatedAt: new Date(),
+      },
+      {
+        id: '55894438-2b73-4c15-8a7b-b10d54692c95',
+        codeCompletionMiniGameId: '6256133f-a0cb-4fec-b25c-520595ddf884',
+        content: 'as',
+        isCorrect: false,
+        createdAt: new Date(),
+        updatedAt: new Date(),
+      },
+      {
+        id: 'ddc6af11-c073-4f11-866c-feedb1210f30',
+        codeCompletionMiniGameId: '6256133f-a0cb-4fec-b25c-520595ddf884',
+        content: 'if',
+        isCorrect: false,
+        createdAt: new Date(),
+        updatedAt: new Date(),
+      },
+    ],
+    //end Code Completion - 1;
+
+    // Code Completion - 2 (c72d7fe7-8a45-433a-9cab-f625248c53e6)
+    ...[
+      {
+        id: 'bc59bf0b-18e3-48e7-a400-54d6e9e9cef6',
+        codeCompletionMiniGameId: 'c72d7fe7-8a45-433a-9cab-f625248c53e6',
+        content: 'elif',
+        isCorrect: false,
+        createdAt: new Date(),
+        updatedAt: new Date(),
+      },
+      {
+        id: 'cc7eda8e-5914-4c4d-8107-6a3db53745cf',
+        codeCompletionMiniGameId: 'c72d7fe7-8a45-433a-9cab-f625248c53e6',
+        content: 'else',
+        isCorrect: true,
+        createdAt: new Date(),
+        updatedAt: new Date(),
+      },
+      {
+        id: '8ef3e9ba-c4e1-4e62-bebe-2dd1520f61bf',
+        codeCompletionMiniGameId: 'c72d7fe7-8a45-433a-9cab-f625248c53e6',
+        content: 'while',
+        isCorrect: false,
+        createdAt: new Date(),
+        updatedAt: new Date(),
+      },
+      {
+        id: '13669058-7443-43a5-88d3-2fdaffa13bf2',
+        codeCompletionMiniGameId: 'c72d7fe7-8a45-433a-9cab-f625248c53e6',
+        content: 'or',
+        isCorrect: false,
+        createdAt: new Date(),
+        updatedAt: new Date(),
+      },
+      {
+        id: '98d6635f-7724-450f-9efe-5bfffad0566f',
+        codeCompletionMiniGameId: 'c72d7fe7-8a45-433a-9cab-f625248c53e6',
+        content: 'if',
+        isCorrect: false,
+        createdAt: new Date(),
+        updatedAt: new Date(),
+      },
+    ],
+    //end Code Completion - 2;
+  ];
 
 export const module2TrueFalseMiniGames: TrueFalseMiniGame[] = [
   {
@@ -42,6 +156,18 @@ export const module2MiniGames: MiniGame[] = [
   // Level 1: Declarações condicionais (if, else if, else)
   //
   {
+    id: '0693acc8-a11a-4e0e-8c73-caaaa2115065',
+    codeCompletionMiniGameId: null,
+    trueFalseMiniGameId: null,
+    codeOrderingMiniGameId: null,
+    markdownMiniGameId: module2MarkDownMiniGames[0].id,
+    createdAt: new Date(),
+    updatedAt: new Date(),
+    levelId: levels[2].id,
+    type: 'MARKDOWN',
+    orderNumber: 0,
+  },
+  {
     id: '8a9e7e07-d5ae-4c80-88c6-e35dad3370bf',
     codeCompletionMiniGameId: null,
     trueFalseMiniGameId: module2TrueFalseMiniGames[0].id,
@@ -49,8 +175,68 @@ export const module2MiniGames: MiniGame[] = [
     markdownMiniGameId: null,
     createdAt: new Date(),
     updatedAt: new Date(),
-    levelId: levels[4].id,
+    levelId: levels[2].id,
     type: 'TRUE_FALSE',
+    orderNumber: 0,
+  },
+  {
+    id: '19fd91d4-bc30-4400-93c1-553d9e65421a',
+    codeCompletionMiniGameId: null,
+    trueFalseMiniGameId: module2TrueFalseMiniGames[1].id,
+    codeOrderingMiniGameId: null,
+    markdownMiniGameId: null,
+    createdAt: new Date(),
+    updatedAt: new Date(),
+    levelId: levels[2].id,
+    type: 'TRUE_FALSE',
+    orderNumber: 0,
+  },
+  {
+    id: '436934d8-e4b4-4abe-a0f8-f479d1b31676',
+    codeCompletionMiniGameId: null,
+    trueFalseMiniGameId: module2TrueFalseMiniGames[2].id,
+    codeOrderingMiniGameId: null,
+    markdownMiniGameId: null,
+    createdAt: new Date(),
+    updatedAt: new Date(),
+    levelId: levels[2].id,
+    type: 'TRUE_FALSE',
+    orderNumber: 0,
+  },
+  {
+    id: 'cf37559c-1a5f-4b54-b4a1-a97f536a00d6',
+    codeCompletionMiniGameId: module2CodeCompletionMiniGames[0].id,
+    trueFalseMiniGameId: null,
+    codeOrderingMiniGameId: null,
+    markdownMiniGameId: null,
+    createdAt: new Date(),
+    updatedAt: new Date(),
+    levelId: levels[2].id,
+    type: 'CODE_COMPLETION',
+    orderNumber: 0,
+  },
+  {
+    id: 'b87b5cf6-8f96-47f6-923f-84adde764486',
+    codeCompletionMiniGameId: module2CodeCompletionMiniGames[1].id,
+    trueFalseMiniGameId: null,
+    codeOrderingMiniGameId: null,
+    markdownMiniGameId: null,
+    createdAt: new Date(),
+    updatedAt: new Date(),
+    levelId: levels[2].id,
+    type: 'CODE_COMPLETION',
+    orderNumber: 0,
+  },
+  {
+    id: 'b87b5cf6-8f96-47f6-923f-84adde764486',
+    codeCompletionMiniGameId: null,
+    trueFalseMiniGameId: null,
+    codeOrderingMiniGameId: null,
+    markdownMiniGameId: null,
+    createdAt: new Date(),
+    updatedAt: new Date(),
+    levelId: levels[2].id,
+    type: 'CODE_ORDERING',
     orderNumber: 0,
   },
   // end Level 1;
