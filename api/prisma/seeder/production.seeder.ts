356afd13183ca52f48d514d573f49353c41aaa68
Lucas <lucastsantos.developer@gmail.com>
2023-10-29 23:15:00 -0300

    chore: adds prod seed

--- api/prisma/seeder/production.seeder.ts
+++ api/prisma/seeder/production.seeder.ts
@@ -1,9 +1,104 @@
 import { PrismaClient } from '@prisma/client';
+import { images } from './data/images';
+import { programmingLanguages } from './data/languages';
+import { levels } from './data/levels';
+import {
+  trueFalseMiniGames,
+  codeCompletionMiniGames,
+  codeCompletionMiniGameOptions,
+  codeOrderingMiniGames,
+  codeOrderingMiniGameOptions,
+  markDownMiniGames,
+  miniGames,
+} from './data/minigames';
+import { modules } from './data/modules';
+import { students } from './data/students';
 
 export class SeedProduction {
   constructor(private readonly prisma: PrismaClient) {}
 
   async execute(): Promise<void> {
-    console.log('SeedProduction');
+    console.log('Seeding database... [!PROD!]');
+
+    // CREATE images
+    await this.prisma.image.createMany({
+      data: images,
+      skipDuplicates: true,
+    });
+
+    // CREATE programmingLanguages
+    await this.prisma.programmingLanguage.createMany({
+      data: programmingLanguages,
+      skipDuplicates: true,
+    });
+
+    // CREATE modules
+    await this.prisma.module.createMany({
+      data: modules,
+      skipDuplicates: true,
+    });
+
+    // CREATE levels
+    await this.prisma.level.createMany({
+      data: levels,
+      skipDuplicates: true,
+    });
+
+    // CREATE trueFalseMiniGames
+    await this.prisma.trueFalseMiniGame.createMany({
+      data: trueFalseMiniGames,
+      skipDuplicates: true,
+    });
+
+    // CREATE codeCompletionMiniGames
+    await this.prisma.codeCompletionMiniGame.createMany({
+      data: codeCompletionMiniGames,
+      skipDuplicates: true,
+    });
+
+    // CREATE codeCompletionMiniGameOptions
+    await this.prisma.codeCompletionMiniGameOption.createMany({
+      data: codeCompletionMiniGameOptions,
+      skipDuplicates: true,
+    });
+
+    // CREATE codeOrderingMiniGames
+    await this.prisma.codeOrderingMiniGame.createMany({
+      data: codeOrderingMiniGames,
+      skipDuplicates: true,
+    });
+
+    // CREATE codeOrderingMiniGameOptions
+    await this.prisma.codeOrderingMiniGameOption.createMany({
+      data: codeOrderingMiniGameOptions,
+      skipDuplicates: true,
+    });
+
+    // CREATE MARKDOWN MINIGAMES
+    await this.prisma.markdownMiniGame.createMany({
+      data: markDownMiniGames,
+      skipDuplicates: true,
+    });
+
+    // CREATE minigames
+    await this.prisma.miniGame.createMany({
+      data: miniGames,
+      skipDuplicates: true,
+    });
+
+    const copyStudents = students;
+
+    for (const student of copyStudents) {
+      student.password =
+        '$2a$12$SVcymT2MxaeDtjo6OvFS6uUyWOh151OdHheE0CyOvPj6Gml0/zkWq'; // Teste123@
+    }
+
+    // CREATE students
+    await this.prisma.student.createMany({
+      data: copyStudents,
+      skipDuplicates: true,
+    });
+
+    console.log('Seeding completed!');
   }
 }
