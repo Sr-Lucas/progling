31ca483a6a09df9a931d4802aa8171032994397f
Lucas <lucastsantos.developer@gmail.com>
2023-11-02 18:42:18 -0300

    refactor: removes requirement for password

--- api/src/app/student/dto/create-student.dto.ts
+++ api/src/app/student/dto/create-student.dto.ts
@@ -16,11 +16,11 @@ export class CreateStudentDto {
   name: string;
 
   @IsString()
-  @MinLength(8, { message: 'Password must be at least 8 characters long' })
-  @Matches(/^(?=.*\d)(?=.*[a-z])(?=.*[A-Z])(?=.*[^a-zA-Z0-9]).{8,}$/, {
-    message:
-      'Password must contain at least one uppercase letter, one lowercase letter, one number, and one special character',
-  })
+  @MinLength(6, { message: 'Password must be at least 6 characters long' })
+  // @Matches(/^(?=.*\d)(?=.*[a-z])(?=.*[A-Z])(?=.*[^a-zA-Z0-9]).{8,}$/, {
+  //   message:
+  //     'Password must contain at least one uppercase letter, one lowercase letter, one number, and one special character',
+  // })
   @IsNotEmpty()
   password: string;
 }
